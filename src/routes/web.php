ee9b407d3170bcb4e2555ea2f554eafadcae612f
Ashraf Emon <ashraf.emon143@gmail.com>
2023-02-02 01:49:45 +0600

    Routing bug fix

--- src/routes/web.php
+++ src/routes/web.php
@@ -5,36 +5,36 @@ use Illuminate\Support\Facades\Route;
 Route::prefix('installer')->group(function () {
     Route::redirect('', 'requirements');
 
-    Route::get('requirements', [\Ashraf\LaravelInstaller\Http\Controllers\InstallerController::class, 'requirementsView'])
+    Route::get('requirements', [\Leafwrap\LaravelInstaller\Http\Controllers\InstallerController::class, 'requirementsView'])
         ->name('requirements.index');
-    Route::post('requirements', [\Ashraf\LaravelInstaller\Http\Controllers\InstallerController::class, 'requirementsStore'])
+    Route::post('requirements', [\Leafwrap\LaravelInstaller\Http\Controllers\InstallerController::class, 'requirementsStore'])
         ->name('requirements.store');
 
-    Route::get('permissions', [\Ashraf\LaravelInstaller\Http\Controllers\InstallerController::class, 'permissionsView'])
+    Route::get('permissions', [\Leafwrap\LaravelInstaller\Http\Controllers\InstallerController::class, 'permissionsView'])
         ->name('permissions.index');
-    Route::post('permissions', [\Ashraf\LaravelInstaller\Http\Controllers\InstallerController::class, 'permissionsStore'])
+    Route::post('permissions', [\Leafwrap\LaravelInstaller\Http\Controllers\InstallerController::class, 'permissionsStore'])
         ->name('permissions.store');
 
-    Route::get('license', [\Ashraf\LaravelInstaller\Http\Controllers\InstallerController::class, 'licenseView'])
+    Route::get('license', [\Leafwrap\LaravelInstaller\Http\Controllers\InstallerController::class, 'licenseView'])
         ->name('license.index');
-    Route::post('license', [\Ashraf\LaravelInstaller\Http\Controllers\InstallerController::class, 'licenseStore'])
+    Route::post('license', [\Leafwrap\LaravelInstaller\Http\Controllers\InstallerController::class, 'licenseStore'])
         ->name('license.store');
 
-    Route::get('database', [\Ashraf\LaravelInstaller\Http\Controllers\InstallerController::class, 'databaseView'])
+    Route::get('database', [\Leafwrap\LaravelInstaller\Http\Controllers\InstallerController::class, 'databaseView'])
         ->name('database.index');
-    Route::post('database', [\Ashraf\LaravelInstaller\Http\Controllers\InstallerController::class, 'databaseStore'])
+    Route::post('database', [\Leafwrap\LaravelInstaller\Http\Controllers\InstallerController::class, 'databaseStore'])
         ->name('database.store');
 
-    Route::get('mail', [\Ashraf\LaravelInstaller\Http\Controllers\InstallerController::class, 'mailView'])
+    Route::get('mail', [\Leafwrap\LaravelInstaller\Http\Controllers\InstallerController::class, 'mailView'])
         ->name('mail.index');
-    Route::post('mail', [\Ashraf\LaravelInstaller\Http\Controllers\InstallerController::class, 'mailStore'])
+    Route::post('mail', [\Leafwrap\LaravelInstaller\Http\Controllers\InstallerController::class, 'mailStore'])
         ->name('mail.store');
 
-    Route::get('install', [\Ashraf\LaravelInstaller\Http\Controllers\InstallerController::class, 'installView'])
+    Route::get('install', [\Leafwrap\LaravelInstaller\Http\Controllers\InstallerController::class, 'installView'])
         ->name('install.index');
-    Route::post('install', [\Ashraf\LaravelInstaller\Http\Controllers\InstallerController::class, 'installStore'])
+    Route::post('install', [\Leafwrap\LaravelInstaller\Http\Controllers\InstallerController::class, 'installStore'])
         ->name('install.store');
 
-    Route::get('finish', [\Ashraf\LaravelInstaller\Http\Controllers\InstallerController::class, 'finishView'])
+    Route::get('finish', [\Leafwrap\LaravelInstaller\Http\Controllers\InstallerController::class, 'finishView'])
         ->name('finish.index');
 });
--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -6,21 +6,19 @@ namespace Composer\Autoload;
 
 class ComposerStaticInit1e65ffe3633b250109fe706844a16cc3
 {
-    public static $prefixLengthsPsr4 = array (
-        'A' => 
-        array (
-            'Ashraf\\LaravelInstaller\\' => 24,
+    public static $prefixLengthsPsr4 = array(
+        'A' => array(
+            'Leafwrap\\LaravelInstaller\\' => 24,
         ),
     );
 
-    public static $prefixDirsPsr4 = array (
-        'Ashraf\\LaravelInstaller\\' => 
-        array (
+    public static $prefixDirsPsr4 = array(
+        'Leafwrap\\LaravelInstaller\\' => array(
             0 => __DIR__ . '/../..' . '/src',
         ),
     );
 
-    public static $classMap = array (
+    public static $classMap = array(
         'Composer\\InstalledVersions' => __DIR__ . '/..' . '/composer/InstalledVersions.php',
     );
 
@@ -28,8 +26,8 @@ class ComposerStaticInit1e65ffe3633b250109fe706844a16cc3
     {
         return \Closure::bind(function () use ($loader) {
             $loader->prefixLengthsPsr4 = ComposerStaticInit1e65ffe3633b250109fe706844a16cc3::$prefixLengthsPsr4;
-            $loader->prefixDirsPsr4 = ComposerStaticInit1e65ffe3633b250109fe706844a16cc3::$prefixDirsPsr4;
-            $loader->classMap = ComposerStaticInit1e65ffe3633b250109fe706844a16cc3::$classMap;
+            $loader->prefixDirsPsr4    = ComposerStaticInit1e65ffe3633b250109fe706844a16cc3::$prefixDirsPsr4;
+            $loader->classMap          = ComposerStaticInit1e65ffe3633b250109fe706844a16cc3::$classMap;
 
         }, null, ClassLoader::class);
     }
